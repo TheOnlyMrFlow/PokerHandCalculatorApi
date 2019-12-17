0af1743c77839aca1153151afb6d7c89b66b38e4
MrFLow <comte.florian@gmail.com>
2019-12-17 17:23:15 +0100

    process port

--- src/Presentation/server.ts
+++ src/Presentation/server.ts
@@ -19,33 +19,33 @@ Object.keys(CardFace)
     console.log(face);
   });
 
-app.listen(80, () => {
+const port = process.env.PORT;
+app.listen(port || 8080, () => {
   // tslint:disable-next-line:no-console
-  console.log("Listening on port 80 ...");
-});
+  console.log("Listening on port" + port);
 
-app.post("/solveround", (req, res, next) => {
-  // res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
-  // tslint:disable-next-line:no-console
-  console.log(req.body);
+  app.post("/solveround", (req, res, next) => {
+    // res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    // tslint:disable-next-line:no-console
+    console.log(req.body);
 
-  const players = new Map<string, string>();
+    const players = new Map<string, string>();
 
-  req.body.players.forEach((p: { name: string; cards: string; }) => {
-    players.set(p.name, p.cards);
+    req.body.players.forEach((p: { name: string; cards: string; }) => {
+      players.set(p.name, p.cards);
+    });
+
+    const round: Round = new Round(players, req.body.community_cards);
+
+    const response: any = {};
+    let i = 0;
+    round.OrderedPlayers.forEach((p: Player) => {
+      response[p.name] = {
+        combinations: p.hand.BestFiveCards,
+        rank: i
+      };
+      i++;
+    });
+
+    res.json(response);
   });
-
-  const round: Round = new Round(players, req.body.community_cards);
-
-  const response: any = {};
-  let i = 0;
-  round.OrderedPlayers.forEach((p: Player) => {
-    response[p.name] = {
-      combinations: p.hand.BestFiveCards,
-      rank: i
-    };
-    i++;
-  });
-
-  res.json(response);
-});
