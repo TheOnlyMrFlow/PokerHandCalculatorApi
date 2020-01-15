e402fbc7bb38766ef9eb3a88b89279a79c0b4b87
MrFLow <comte.florian@gmail.com>
2020-01-15 16:04:35 +0100

    gestion d'egalite

--- src/Business/Round.ts
+++ src/Business/Round.ts
@@ -1,3 +1,4 @@
+import { Card } from "./Card";
 import { Hand } from "./Hand";
 import { Player } from "./Player";
 
@@ -28,6 +29,20 @@ class Round {
                 console.log(p2.hand.BestFiveCards[i][0]);
                 if (diff !== 0) {
                     return diff;
+                }
+
+                const combP1: Card[] = Object.assign([], p1.hand.BestFiveCards[i][1].length);
+                const combP2: Card[] = Object.assign([], p2.hand.BestFiveCards[i][1].length);
+                const length = combP1.length;
+                const combP1Values: number[] = combP1.map(c => c.ValueOfFace);
+                const combP2Values: number[] = combP2.map(c => c.ValueOfFace);
+                combP1Values.sort((one, two) => (one > two ? -1 : 1));
+                combP2Values.sort((one, two) => (one > two ? -1 : 1));
+                for (let j = 0; j < length; j++) {
+                    const diffCard = combP2Values[j] - combP1Values[j];
+                    if (diffCard !== 0) {
+                        return diff;
+                    }
                 }
             }
             return 0;
